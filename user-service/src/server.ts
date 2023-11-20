f221d87566f9c0cd031ab1fc7534d0d1de900481
Snoopsie1 <oliverrasoli@gmail.com>
2023-11-20 17:08:56 +0100

    port change

--- user-service/src/server.ts
+++ user-service/src/server.ts
@@ -1,7 +1,7 @@
 // src/server.ts
 import app from './app';
 
-const port = 3000;
+const port = 8080;
 
 app.listen(port, () => {
   console.log(`User Service listening at http://localhost:${port}`);
