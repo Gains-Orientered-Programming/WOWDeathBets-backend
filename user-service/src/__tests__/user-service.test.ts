b50786be845dc42bd67263162661fdde24fb846d
Snoopsie1 <oliverrasoli@gmail.com>
2023-11-22 12:49:14 +0100

    github secrets and seperate test suites for tests

--- user-service/src/__tests__/user-service.test.ts
+++ user-service/src/__tests__/user-service.test.ts
@@ -1,12 +1,11 @@
 import mongoose from 'mongoose';
 import { beforeAll, afterAll, describe, expect, it } from '@jest/globals';
-import { createUser, getUserByUsername, getUserByEmail } from '../services/user-services';
+import { createUser, getUserByUsername, getUserByEmail, deleteUserById } from '../services/user-services';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-// const mongodbURI = process.env.MONGODB_URI || '';
-const mongodbURI = 'mongodb+srv://users:Eq6ylDvIvlmgs6AN@users.jg9w7bo.mongodb.net/';
+const mongodbURI = process.env.MONGODB_URI || '';
 
 beforeAll(async () => {
   await mongoose.connect(mongodbURI);
@@ -17,40 +16,49 @@ afterAll(async () => {
 });
 
 describe('User Service Tests', () => {
-  it('should create a new user', async () => {
-    const userData = {
-      username: 'testuser',
-      email: 'test@example.com',
-      password: 'testpassword',
-    };
+  describe('Create User', () => {
+    it('should create a new user', async () => {
+      const userData = {
+        username: 'testuser',
+        email: 'test@example.com',
+        password: 'testpassword',
+      };
 
-    const newUser = await createUser(userData);
+      const newUser = await createUser(userData);
 
-    expect(newUser).toBeDefined();
-    expect(newUser.username).toBe(userData.username);
-    expect(newUser.email).toBe(userData.email);
+      expect(newUser).toBeDefined();
+      expect(newUser.username).toBe(userData.username);
+      expect(newUser.email).toBe(userData.email);
+      await deleteUserById(newUser._id);
+    });
   });
 
-  it('should get a user by their username', async () => {
-    const username = 'testuser';
-    const user = await getUserByUsername(username);
+  describe('Get User by Username', () => {
+    it('should get a user by their username', async () => {
+      const username = 'testuser';
+      const user = await getUserByUsername(username);
 
-    expect(user).toBeDefined();
-    expect(user?.username).toBe(username);
+      expect(user).toBeDefined();
+      expect(user?.username).toBe(username);
+    });
   });
 
-  it('should get a user by their email', async () => {
-    const email = 'test@example.com';
-    const user = await getUserByEmail(email);
+  describe('Get User by Email', () => {
+    it('should get a user by their email', async () => {
+      const email = 'test@example.com';
+      const user = await getUserByEmail(email);
 
-    expect(user).toBeDefined();
-    expect(user?.email).toBe(email);
+      expect(user).toBeDefined();
+      expect(user?.email).toBe(email);
+    });
   });
 
-  it('should handle user not found', async () => {
-    const nonExistentUsername = 'nonexistentuser';
-    const user = await getUserByUsername(nonExistentUsername);
+  describe('Handle User Not Found', () => {
+    it('should handle user not found', async () => {
+      const nonExistentUsername = 'nonexistentuser';
+      const user = await getUserByUsername(nonExistentUsername);
 
-    expect(user).toBeNull();
+      expect(user).toBeNull();
+    });
   });
 });
--- user-service/src/services/user-services.ts
+++ user-service/src/services/user-services.ts
@@ -1,5 +1,6 @@
 // src/services/user-service.ts
 import User from '../models/user-model';
+import mongoose, { Types } from 'mongoose';
 import bcrypt from 'bcrypt';
 
 // Function to create a new user
@@ -19,6 +20,25 @@ export const getUserByUsername = async (username: string) => {
   try {
     const user = await User.findOne({ username });
     return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Function to delete a user by their id
+export const deleteUserById = async (id: string) => {
+  try {
+    const objectId = new Types.ObjectId(id);
+    await User.deleteOne({ _id: objectId });
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Function to delete a user by their username
+export const deleteUserByUsername = async (username: string) => {
+  try {
+    await User.deleteMany({ username });
   } catch (error) {
     throw error;
   }
