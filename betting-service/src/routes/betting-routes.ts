8e273f4a34dff08543997eb4ec3f42c768c00abd
Mikkel Bentsen <bentsen405@gmail.com>
2023-11-26 20:08:57 +0100

    yallah

--- betting-service/src/routes/betting-routes.ts
+++ betting-service/src/routes/betting-routes.ts
@@ -1,16 +1,78 @@
-// src/routes/user-routes.ts
-import express from 'express';
 import {
   createBettingController,
   getBettingByIdController,
   deleteBettingByIdController,
 } from '../controllers/betting-controller';
+import { Express } from 'express';
 
-const router = express.Router();
+function routes(app: Express) {
+  /**
+   * @openapi
+   * /bettings/create-betting:
+   *  post:
+   *   summary: Create a new betting
+   *   tags: [Betting]
+   *   requestBody:
+   *    required: true
+   *    content:
+   *     application/json:
+   *      schema:
+   *       type: object
+   *       required:
+   *        - characterName
+   *        - region
+   *        - realm
+   *        - amount
+   *       properties:
+   *        characterName:
+   *         type: string
+   *         default: petrice
+   *        region:
+   *         type: string
+   *         default: eu
+   *        realm:
+   *         type: string
+   *         default: nekrosh
+   *        amount:
+   *         type: number
+   *         default: 0
+   *      responses:
+   *        200:
+   *          description: The created betting.
+   *          content:
+   *            application/json:
+   *        500:
+   *         description: Internal Server Error
+   */
+  app.post('/create-betting', createBettingController);
 
-// Define user routes
-router.post('/create-betting', createBettingController);
-router.get('/betting/:id', getBettingByIdController);
-router.delete('/betting/:id', deleteBettingByIdController);
+  /**
+   * @openapi
+   * /bettings/{id}:
+   *  get:
+   *   tags: [Betting]
+   *   summary: Get a betting by id
+   *   parameters:
+   *    - in: path
+   *      name: id
+   *      description: The id of the betting
+   *      required: true
+   */
+  app.get('/betting/:id', getBettingByIdController);
 
-export default router;
+  /**
+   * @openapi
+   * /bettings/{id}:
+   *  delete:
+   *   tags: [Betting]
+   *   summary: Delete a betting by id
+   *   parameters:
+   *    - in: path
+   *      name: id
+   *      description: The id of the betting
+   *      required: true
+   */
+  app.delete('/betting/:id', deleteBettingByIdController);
+}
+
+export default routes;
