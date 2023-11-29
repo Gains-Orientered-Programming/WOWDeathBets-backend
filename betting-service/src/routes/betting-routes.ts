39d7cd32af1940b5139972f1c0bc9fc4b897d0ee
Mikkel Bentsen <cph-mb781@cphbusiness.dk>
2023-11-29 16:31:28 +0100

    lort

--- betting-service/src/routes/betting-routes.ts
+++ betting-service/src/routes/betting-routes.ts
@@ -11,7 +11,7 @@ function routes(app: Express) {
   app.get('/bettings', getAllBettingsController as RequestHandler);
   app.post('/bettings', createBettingController as RequestHandler);
   app.get('/bettings/:id', getBettingByIdController as RequestHandler);
-  app.get('/bettings/:userId', getBettingsByUserIdController as RequestHandler);
+  app.get('/bettings/userId/:id', getBettingsByUserIdController as RequestHandler);
   app.delete('/bettings/:id', deleteBettingByIdController as RequestHandler);
 }
 
