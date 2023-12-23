fa9c3dd8f33e7d819c7c003cdbb24856283499cc
Mikkel Bentsen <cph-mb781@cphbusiness.dk>
2023-12-23 21:12:26 +0100

    Kafka updates

--- betting-service/src/kafka/producer.ts
+++ betting-service/src/kafka/producer.ts
@@ -1,5 +1,5 @@
 import { KafkaClient, Producer, ProduceRequest } from 'kafka-node';
-
+import cron from 'node-cron';
 const client = new KafkaClient({ kafkaHost: 'localhost:9092' });
 const producer = new Producer(client);
 
@@ -10,10 +10,15 @@ const message = {
 
 const payloads: ProduceRequest[] = [{ topic: 'user_transactions', messages: JSON.stringify(message) }];
 
-producer.send(payloads, (err, data) => {
-  if (err) {
-    console.error('Error sending message:', err);
-  } else {
-    console.log('Message sent:', data);
-  }
+//TODO should check the betting database for resovled bettings and send
+// the results to the user database
+//runs every 30 minutes
+cron.schedule('*/30 * * * *', async () => {
+  producer.send(payloads, (err, data) => {
+    if (err) {
+      console.error('Error sending message:', err);
+    } else {
+      console.log('Message sent:', data);
+    }
+  });
 });
